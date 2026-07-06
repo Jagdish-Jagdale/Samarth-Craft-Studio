433a41ce90aad482dc9f07d653490de4a4860d62
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-07-06 23:08:51 +0530

    remove ab in product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -24,7 +24,7 @@ const reviews = [
   },
   {
     name: 'Vikram S.',
-    tag: 'VERIFIED COLLECTORab',
+    tag: 'VERIFIED COLLECTOR',
     date: 'Aug 2023',
     quote: '"Exceeded all expectations."',
     text: 'I\'ve bought many luxury brands, but the artisanal soul of Samartha is unmatched. The delivery experience was also incredibly premium.',

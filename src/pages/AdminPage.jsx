9cdd55c2a7e853919cdc14a035a7fb36641b1ff4
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-09-11 09:26:53 +0530

    added ot in admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -30,7 +30,7 @@ const initialProducts = [
   },
   {
     id: 2,
-    name: "Midnight Zardosi Silk Jutti",
+    name: "Midnight Zardosi Silk Juttiot",
     sku: "SM-JUT-082",
     category: "Kolhapuri Chappal",
     collection: "Heritage Earth",
